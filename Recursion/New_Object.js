e8d770d5e554ac79216241121705b65be61b6fb4
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-06-25 09:19:38 +0530

    Convert numbers in a nested object into string using recursion

---
+++ Recursion/New_Object.js
@@ -0,0 +1,33 @@
+function stringifyNumbers(obj) {
+  let newObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (
+      typeof obj[key] === "object" &&
+      obj[key] !== null &&
+      !Array.isArray(obj[key])
+    ) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
+
+console.log(stringifyNumbers(obj));
