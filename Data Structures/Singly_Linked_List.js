2012e3bada0002842497a2b829b4f00b439e625b
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-01 10:51:51 +0530

    Singly linked list push operation

---
+++ Data Structures/Singly_Linked_List.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.tail = null;
+    this.head = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+console.log(list.push("Hi"));
+
+console.log(list.push("Jishnu"));
+
+console.log(list.push("How are you"));
