58d632a97ef9a0f70bc36e6499f567a83beb27ec
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-01 13:26:06 +0530

    Shift and Unshift operation in Singly linked list

--- Data Structures/Singly_Linked_List.js
+++ Data Structures/Singly_Linked_List.js
@@ -1,5 +1,3 @@
-const { c } = require("tar");
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -13,6 +11,7 @@ class SinglyLinkedList {
     this.head = null;
     this.length = 0;
   }
+  // Method to add a new node to the end of the list
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
@@ -25,6 +24,7 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  // Method to remove and return the last node of the list
   pop() {
     if (this.length === 0) return undefined;
     let currentNode = this.head;
@@ -42,6 +42,32 @@ class SinglyLinkedList {
     }
     return currentNode;
   }
+  // Method to remove and return the first node of the list
+  shift() {
+    if (this.length === 0) return undefined;
+    let previousHead = this.head;
+    this.head = previousHead.next;
+    previousHead.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return previousHead;
+  }
+  // Method to add a new node at the beginning of the list
+  unshift(val) {
+    let newHead = new Node(val);
+    if (this.length === 0) {
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      newHead.next = this.head;
+      this.head = newHead;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
@@ -51,14 +77,10 @@ console.log(list.push("Jishnu"));
 
 console.log(list.push("How are you"));
 
-list.pop();
-
+list.shift();
 console.log(list);
 
-list.pop();
-
+list.shift();
 console.log(list);
 
-list.pop();
-
-console.log(list);
+console.log(list.unshift(1));
