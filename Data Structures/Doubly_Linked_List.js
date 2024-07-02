bfb510dcc606bf67f9d7d8e18819586610eb20ab
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-02 10:21:18 +0530

    Push operation in Doubly linked list.

---
+++ Data Structures/Doubly_Linked_List.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.tail = null;
+    this.head = null;
+    this.length = 0;
+  }
+  push(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new DoublyLinkedList();
+console.log(list.push(1));
+console.log(list.push(3));
