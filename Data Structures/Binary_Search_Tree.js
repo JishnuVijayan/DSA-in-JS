b51095b686856f0dd95c66034a13737e1f07acd1
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-04 12:44:47 +0530

    Insert method in Binary Search Tree.

--- Data Structures/Binary_Search_Tree.js
+++ Data Structures/Binary_Search_Tree.js
@@ -10,9 +10,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    let condition = true;
+    while (condition) {
+      if (currentNode.value > newNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          condition = false;
+        } else currentNode = currentNode.left;
+      } else if (currentNode.value < newNode.value) {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          condition = false;
+        } else currentNode = currentNode.right;
+      } else return false;
+    }
+    return this;
+  }
 }
 
 var tree = new BinarySearchTree();
-tree.root = new Node(30);
-tree.root.left = new Node(20);
-tree.root.right = new Node(60);
+tree.insert(10);
+tree.insert(5);
+tree.insert(2);
+tree.insert(13);
+tree.insert(7);
+tree.insert(11);
+console.log(tree.insert(16));
