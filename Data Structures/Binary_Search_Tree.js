41a11a92a3a53b6587eedb29976641ce37a05d60
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-04 16:57:35 +0530

    BFS

--- Data Structures/Binary_Search_Tree.js
+++ Data Structures/Binary_Search_Tree.js
@@ -48,14 +48,28 @@ class BinarySearchTree {
 
     return false;
   }
+  BFS() {
+    let queue = [], //instead of making a queue from scratch, we use array for simplicity of code.
+      visited = []; // we return this at last.
+    queue.push(this.root);
+    while (queue.length > 0) {
+      let node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      visited.push(node.value);
+    }
+    return visited;
+  }
 }
 
 var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(2);
-tree.insert(13);
-tree.insert(7);
-tree.insert(11);
-tree.insert(16);
-console.log(tree.find(16));
+tree.insert(6);
+tree.insert(3);
+tree.insert(8);
+tree.insert(15);
+tree.insert(20);
+//tree.insert(70);
+//console.log(tree.find(16));
+
+console.log(tree.BFS());
