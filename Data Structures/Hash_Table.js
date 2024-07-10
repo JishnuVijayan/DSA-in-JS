ed05e4935e9b4ebdd3504f8285bde98c3cd86376
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-10 17:07:36 +0530

    key, values method in Hash table

--- Data Structures/Hash_Table.js
+++ Data Structures/Hash_Table.js
@@ -27,19 +27,48 @@ class HashTable {
     }
     return undefined;
   }
+  keys() {
+    let keyValues = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keyValues.includes(this.keyMap[i][j][0])) {
+            // handles duplicate
+            keyValues.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keyValues;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            // handles duplicate
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
 
 let ht = new HashTable();
 ht.set("pink", "#ffc0cb");
 ht.set("cyan", "#00ffff");
 ht.set("maroon", "#800000");
+ht.set("maroon", "#800000");
 console.log(ht.keyMap);
 console.log(ht.get("pink"));
 console.log(ht.get("cyan"));
 console.log(ht.get("maroon"));
-console.log(ht.get("purple")); 
-
-
+console.log(ht.get("purple"));
+console.log(ht.keys());
+console.log(ht.values());
 
 
 
