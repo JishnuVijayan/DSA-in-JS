eeec27199a68d29154b854880ba6c50d70faefd9
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-11 14:05:19 +0530

    Methods for removing edge and vertex in undirected graph

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -1,4 +1,4 @@
-// Undirected graph
+// undirected graph
 class Graph {
   constructor() {
     this.adjacencyList = {};
@@ -18,6 +18,25 @@ class Graph {
       console.log("One or both vertices do not exist.");
     }
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+    }
+  }
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return;
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 let g = new Graph();
@@ -27,4 +46,7 @@ g.addVertex("South Africa");
 console.log(g);
 g.addEdge("India", "South Africa");
 g.addEdge("India", "UK");
+console.log(g);
+// g.removeEdge("India", "South Africa");
+g.removeVertex("South Africa");
 console.log(g);
