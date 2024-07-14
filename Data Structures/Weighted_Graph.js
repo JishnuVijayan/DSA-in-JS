b69ccaed18b378de9bf34279df562fa0803cc2ba
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-07-14 17:00:34 +0530

    Improved version of Dijkstra's algorithm

--- Data Structures/Weighted_Graph.js
+++ Data Structures/Weighted_Graph.js
@@ -1,18 +1,83 @@
-// remove the comments in case of doubt.
-
-class NaivePriorityQueue {
+// class NaivePriorityQueue {
+//   constructor() {
+//     this.values = [];
+//   }
+//   enqueue(value, priority) {
+//     this.values.push({ value, priority });
+//     this.sort();
+//   }
+//   dequeue() {
+//     return this.values.shift();
+//   }
+//   sort() {
+//     this.values.sort((a, b) => a.priority - b.priority);
+//   }
+// }
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+class PriorityQueue {
   constructor() {
     this.values = [];
   }
   enqueue(value, priority) {
-    this.values.push({ value, priority });
-    this.sort();
+    let newNode = new Node(value, priority);
+    this.values.push(newNode);
+    this.bubbleup();
+  }
+  bubbleup() {
+    if (this.values.length === 1) return this;
+    let newNodeIndex = this.values.length - 1;
+    let element = this.values[newNodeIndex];
+    while (newNodeIndex > 0) {
+      let parentIndex = Math.floor((newNodeIndex - 1) / 2);
+      let parent = this.values[parentIndex];
+
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[newNodeIndex] = parent;
+      newNodeIndex = parentIndex;
+    }
   }
   dequeue() {
-    return this.values.shift();
+    let length = this.values.length;
+    if (length === 0) return null;
+    if (length === 1) return this.values.pop();
+    let end = this.values.pop();
+    let min = this.values[0];
+    this.values[0] = end;
+    this.bubbleDown();
+    return min;
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  bubbleDown() {
+    let index = 0;
+    let length = this.values.length;
+    let element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let swap = null;
+      let leftChild, rightChild;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority < element.priority) swap = leftChildIndex;
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        )
+          swap = rightChildIndex;
+      }
+      if (swap == null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
   }
 }
 
@@ -30,7 +95,7 @@ class WeightedGraph {
     }
   }
   Dijkstras(start, end) {
-    let nodes = new NaivePriorityQueue();
+    let nodes = new PriorityQueue();
     let distances = {};
     let previous = {};
     let currentSmallest;
@@ -95,4 +160,4 @@ wg.addEdge("C", "F", 4);
 wg.addEdge("D", "E", 3);
 wg.addEdge("D", "F", 1);
 wg.addEdge("E", "F", 1);
-console.log(wg.Dijkstras("A", "D"));
+console.log(wg.Dijkstras("A", "E"));
